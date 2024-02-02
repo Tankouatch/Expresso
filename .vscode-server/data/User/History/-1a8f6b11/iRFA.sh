a0688e8f3635d45526ddb646d5b08effb6854363
Tankoua <tankouab@gmail.com>
2024-02-02 13:16:29 -0600

    Delete .vscode-server directory

--- .vscode-server/data/User/History/-1a8f6b11/iRFA.sh
+++
@@ -1,5 +0,0 @@
-kubectl patch crd applicationsets.argoproj.io --type=json -p='[
-    {"op": "add", "path": "/metadata/labels/app.kubernetes.io~1managed-by", "value": "Helm"},
-    {"op": "add", "path": "/metadata/annotations/meta.helm.sh~1release-name", "value": "argo-cd"},
-    {"op": "add", "path": "/metadata/annotations/meta.helm.sh~1release-namespace", "value": "s4tankoua"}
-]'
